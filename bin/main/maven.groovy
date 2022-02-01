84ea717088d6c918ed948a9193cc671e865a1127
Diego Inostroza <diego.inoca@gmail.com>
2022-01-31 21:40:04 -0300

    bug fix maven

--- bin/main/maven.groovy
+++ bin/main/maven.groovy
@@ -19,10 +19,9 @@ def call(){
           sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
       }
   }
-  stage("Paso 5: Curl Springboot Gradle sleep 20"){
-      sh "gradle bootRun&"
-      sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-  }
+    stage("Paso 5: Levantar Springboot APP"){
+        sh 'mvn spring-boot:run &'
+    }
   stage("Paso 6: Subir Nexus"){
       nexusPublisher nexusInstanceId: 'nexus',
       nexusRepositoryId: 'devops-usach-nexus',
